aab442954ec71518c29da697a92ad59ebc1e027b
Bruna de Paiva Gonçalves <65919877+Pip0ca@users.noreply.github.com>
2020-07-23 18:36:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-const formulario = document.getElementById("formulario");
-const email = document.getElementById("email");
+function enviar(){
+  var email= document.querySelector('#email');
+  var error = document.querySelector('#error-email');
 
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const emailVal = email.value;
-
-  if (!validateEmail(emailVal)) {
-    formulario.classList.add("error");
-    alert("Insert a valid Email :(");
-  } else {
-    formulario.classList.remove("error");
-    alert("Thanks for seeing my challenge solution :)");
+  if(!email.checkValidity()){
+    error.innerHTML = "Please provide a valid email address";
   }
-});
+}
 
-function validateEmail(email) {
-  var re = /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+))|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}const formulario = document.getElementById("formulario");
-const email = document.getElementById("email");
+function redefinirMsg(){
+  var error = document.querySelector('#error-email');
 
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const emailVal = email.value;
-
-  if (!validateEmail(emailVal)) {
-    formulario.classList.add("error");
-    alert("Insert a valid Email :(");
-  } else {
-    formulario.classList.remove("error");
-    alert("Thanks for seeing my challenge solution :)");
-  }
-});
-
-function validateEmail(email) {
-  var re = /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+))|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if(error.innerHTML == "Please provide a valid email address"){
+    error.innerHTML = "";
+  };
 }
